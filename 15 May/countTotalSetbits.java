a4af1eb01d5421da074755c2b2925f2157a57047
Rahul Agale <78431519+rahulagale@users.noreply.github.com>
2023-05-15 11:02:12 +0530

    Create countTotalSetbits.java

---
+++ 15 May/countTotalSetbits.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static long countBits(long N) {
+        // code here
+        long count=0;
+        for(long i=1;i<=N;i++){
+            count+=setBits(i);
+        }
+        return count;
+    }
+    public static long setBits(long num) {
+        int count = 0;
+      while (num > 0){
+         num &= (num - 1);
+         count++;
+      }
+      return count;
+    }
+}
+ 
