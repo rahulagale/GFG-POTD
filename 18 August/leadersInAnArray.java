dfb05efa2e397772034c1c6a78856db54f4deb1f
Rahul Agale <78431519+rahulagale@users.noreply.github.com>
2023-08-18 09:40:09 +0530

    Create leadersInAnArray.java

---
+++ 18 August/leadersInAnArray.java
@@ -0,0 +1,19 @@
+class Solution{
+    //Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n){
+        // Your code here
+        ArrayList<Integer> result = new ArrayList<>();
+        for(int i=0; i<n;i++){
+            boolean leader = true;
+        
+            for(int j=i+1;j<n;j++)
+                if(arr[j]>arr[i]){
+                    leader = false;
+                    break;
+                }
+                if(leader)result.add(arr[i]);
+            }
+        
+        return result;
+    }
+}
